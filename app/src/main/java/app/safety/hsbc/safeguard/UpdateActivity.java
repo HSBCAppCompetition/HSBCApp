42b7211b35977b094a6125426d17bfb63b8a3e53
lencialam <lencialam@gmail.com>
2017-11-09 13:21:49 +0800

    update activity not finished

---
+++ app/src/main/java/app/safety/hsbc/safeguard/UpdateActivity.java
@@ -0,0 +1,24 @@
+package app.safety.hsbc.safeguard;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class UpdateActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
